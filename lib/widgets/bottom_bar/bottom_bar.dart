f284aa528f3edd1be40bb499851ea86a8cccbe29
Jijgee23 <94855075+Jijgee23@users.noreply.github.com>
2024-11-08 12:50:05 +0800

    sd
    sd

--- lib/widgets/bottom_bar/bottom_bar.dart
+++ lib/widgets/bottom_bar/bottom_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import '../../controllers/home_provider.dart';
 import '../../utilities/colors.dart';
@@ -21,42 +23,47 @@ class _BottomBarState extends State<BottomBar> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final orientation = MediaQuery.of(context).orientation;
-    return Container(
-      margin: EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: (orientation == Orientation.portrait)
-              ? size.width * 0.25
-              : size.width / 3),
-      decoration: BoxDecoration(
+    return SafeArea(
+      top: true,
+      child: Container(
+        margin: EdgeInsets.symmetric(
+            vertical: Platform.isIOS ? 0 : 20,
+            horizontal: (orientation == Orientation.portrait)
+                ? size.width * 0.25
+                : size.width / 3),
+        padding:const EdgeInsets.symmetric(vertical: 0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(color: Colors.grey.shade500, blurRadius: 10)
+            ]),
+        child: ClipRRect(
           borderRadius: BorderRadius.circular(30),
-          color: Colors.white,
-          boxShadow: [BoxShadow(color: Colors.grey.shade500, blurRadius: 10)]),
-      padding: const EdgeInsets.symmetric(vertical: 0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(30),
-        child: BottomNavigationBar(
-            currentIndex: widget.homeProvider.currentIndex,
-            backgroundColor: Colors.white,
-            useLegacyColorScheme: false,
-            showUnselectedLabels: false,
-            showSelectedLabels: false,
-            type: BottomNavigationBarType.fixed,
-            onTap: widget.homeProvider.changeIndex,
-            items: widget.listOfIcons
-                .map(
-                  (i) => BottomNavigationBarItem(
-                    icon: Image.asset(
-                      'assets/icons_2/$i.png',
-                      height: 20,
-                      color: widget.homeProvider.currentIndex ==
-                              widget.listOfIcons.indexOf(i)
-                          ? AppColors.primary.withOpacity(0.9)
-                          : AppColors.primary.withOpacity(.3),
+          child: BottomNavigationBar(
+              currentIndex: widget.homeProvider.currentIndex,
+              backgroundColor: Colors.white,
+              useLegacyColorScheme: false,
+              showUnselectedLabels: false,
+              showSelectedLabels: false,
+              type: BottomNavigationBarType.fixed,
+              onTap: widget.homeProvider.changeIndex,
+              items: widget.listOfIcons
+                  .map(
+                    (i) => BottomNavigationBarItem(
+                      icon: Image.asset(
+                        'assets/icons_2/$i.png',
+                        height: 20,
+                        color: widget.homeProvider.currentIndex ==
+                                widget.listOfIcons.indexOf(i)
+                            ? AppColors.primary.withOpacity(0.9)
+                            : AppColors.primary.withOpacity(.3),
+                      ),
+                      label: widget.labels[widget.listOfIcons.indexOf(i)],
                     ),
-                    label: widget.labels[widget.listOfIcons.indexOf(i)],
-                  ),
-                )
-                .toList()),
+                  )
+                  .toList()),
+        ),
       ),
     );
   }
